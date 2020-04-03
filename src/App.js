66415fde046b32cfa381bb52a953f29278c6e104
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2020-04-03 03:50:24 -0300

    adding footer v030420

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import RuleForm from './pages/RuleForm/index'
+import Footer from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-      <div className=''> 
+      <div id='main-content-container column-item'> 
         <RuleForm/>
       </div>
-      
+      <Footer/> 
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,32 @@
-function Footer(){
-    
+import React from 'react';
+import './styles.css'
+import logoRBT from '../../assets/logos/logorbt.png'
+import fbIcon from '../../assets/logos/fb-icon.png'
+import igIcon from '../../assets/logos/ig-icon.png'
+export default function Footer(){
+    return(
+        <div id='footer-container' className='column-item'>
+            <div className='row-container space-between'>
+                <div className='row-container'>
+                    <div className='item'>
+                        <a href='https://www.rbt.psi.br/'target = "_blank"><img src={logoRBT}/></a>
+                    </div>
+                    <div className='item'>
+                        <a href='https://www.facebook.com/rbtinternet/'target = "_blank"><img className='media' src={fbIcon}/></a>
+                    </div>
+                    <div className='item'>
+                        <a href='https://www.instagram.com/rbt_internet/'target = "_blank"><img className='media' src={igIcon}/></a>
+                    </div>
+                </div>
+                
+                <div className='item'>
+                    
+                </div>
+                <div className='item'></div>
+            </div>
+            <div className='row-container space-around credits'>
+                Developed by Natan Rincker
+            </div>
+        </div>
+    )
 }
